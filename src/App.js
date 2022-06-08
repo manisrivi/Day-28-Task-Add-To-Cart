06b7da43384168c4906ca72d6a4d65d23a188b6a
manisrivi <manisrivi1995@mail.com>
2022-06-08 09:10:32 +0530

    day-28-Task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,191 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import data from "./data";
+import image1 from "./images/image1.jpg";
+import image2 from "./images/image2.jpg";
+import image3 from "./images/image3.jpg";
 
-function App() {
+export default function App() {
+  const [query, setQuery] = useState("");
+  const [cart, setCart] = useState([]);
+  const [productlist, setProductlist] = useState(data);
+
+  let addtocard = (element, id) => {
+    let temp = productlist;
+    temp[id].isDisabled = true;
+    setCart([...cart, element]);
+    setProductlist(temp);
+  };
+  const removefromCart = (element) => {
+    setCart(cart.filter((e) => e !== element));
+    console.log(cart);
+    let index = element.id - 1;
+    let temp = productlist;
+    temp[index].isDisabled = false;
+    setProductlist(temp);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand font-weight-bold">ShopCart</a>
+        <h3 class="text-info">Welcome To ShopCart</h3>
+        <div class="form-inline">
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Search Products"
+            aria-label="Search"
+            onChange={(event) => setQuery(event.target.value)}
+          />
+          {/* cart start */}
+
+          <button
+            type="button"
+            class="btn btn-outline-success"
+            data-toggle="modal"
+            data-target="#exampleModal"
+          >
+            <i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart{" "}
+            {cart.length}
+          </button>
+          <div
+            class="modal fade"
+            id="exampleModal"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">
+                    Cart Items
+                  </h5>
+                  <button
+                    type="button"
+                    class="close"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                  >
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  {/* Remove Item start*/}
+                  <ul className="list-group">
+                    {cart.map((element, index) => (
+                      <li className="list-group-item " key={index}>
+                        <span className="data">
+                          Product Name:{" "}
+                          <span class="text-success font-weight-bold">
+                            {element.name}
+                          </span>
+                        </span>
+                        ,{" "}
+                        <span className="data">
+                          Price:{" "}
+                          <span class="text-success font-weight-bold">
+                          ₹ {element.price}
+                          </span>
+                        </span>
+                        <button
+                          type="button"
+                          className="close"
+                          onClick={() => removefromCart(element)}
+                        >
+                          {" "}
+                          <span aria-hidden="true">&times;</span>
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                  {/* Remove Item End */}
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* cart End */}
+        </div>
+      </nav>
+      {/* Carousel start*/}
+      <div
+        id="carouselExampleIndicators"
+        class="carousel slide"
+        data-ride="carousel"
+      >
+        <ol class="carousel-indicators">
+          <li
+            data-target="#carouselExampleIndicators"
+            data-slide-to="0"
+            class="active"
+          ></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src={image1} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={image2} class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src={image3} class="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-target="#carouselExampleIndicators"
+          data-slide="prev"
         >
-          Learn React
-        </a>
-      </header>
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-target="#carouselExampleIndicators"
+          data-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </button>
+      </div>
+      {/* Carousel End */}
+      <div className="container mt-5">
+        <div className="row">
+          {productlist
+            .filter((g) => g.name.toLowerCase().includes(query))
+            .map((element, index) => (
+              <div className="col-lg-3 col-sm-6 mt-3 mb-3 text-center">
+                {/* card start */}
+                <div class="" style={{ width: "12rem" }}>
+                  <img
+                    src={element.img}
+                    class="card-img-top"
+                    alt="..."
+                    style={{ height: "200px", width: "100px" }}
+                  />
+                  <div class="card-body">
+                    <h6 class="card-title">{element.name}</h6>
+                    <p>{element.rating}</p>
+                    <p class="card-text font-weight-bold">₹ {element.price}</p>
+                    <button
+                      disabled={element.isDisabled}
+                      class="btn btn-warning card_Btn"
+                      onClick={() => addtocard(element, index)}
+                    >
+                      Add To Cart
+                    </button>
+                  </div>
+                </div>
+                {/* card End */}
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/data.js
@@ -0,0 +1,75 @@
+
+const data = [
+    {
+      id: 1,
+      desc: "",
+      name: "Samsung Galaxy M32",
+      rating: "⭐⭐⭐⭐ 4.5",
+      img: "https://rukminim2.flixcart.com/image/312/312/kt4ozgw0/mobile/f/d/s/m32-sm-m326b-sm-m326b-ds-samsung-original-imag6jmvqyef5nyf.jpeg?q=70",
+      isDisabled: false,
+      price: 14499
+    },
+    {
+      id: 2,
+      desc: "",
+      name: "Samsung Galaxy M12",
+      img: "https://rukminim2.flixcart.com/image/312/312/kmjhw280/mobile/h/k/v/galaxy-m12-galaxy-m12-samsung-original-imagffy3wzrmy7ep.jpeg?q=70",
+      isDisabled: false,
+      price: 9999
+    },
+    {
+      id: 3,
+      desc: "",
+      name: "Vivo Y73",
+      rating: "⭐⭐⭐⭐ 4.1",
+      img: "https://m.media-amazon.com/images/I/51emzGhS57S._AC_UL320_.jpg",
+      isDisabled: false,
+      price: 19990
+    },
+    {
+      id: 4,
+      desc: "",
+      name: "OPPO A31",
+      price: 11990,
+      rating: "⭐⭐⭐⭐ 4.2",
+      img: "https://m.media-amazon.com/images/I/71KCwNV6MuL._AC_UL320_.jpg",
+      isSelected: false,
+    },
+    {
+      id: 5,
+      desc: "",
+      name: "Redmi Note 11",
+      price: 9999,
+      rating: "⭐⭐⭐⭐ 4.3",
+      img: "https://rukminim2.flixcart.com/image/312/312/l19m93k0/mobile/o/l/f/note-11-pro-5g-21091116i-redmi-original-imagcvg4ghr3ykge.jpeg?q=70",
+      isSelected: false,
+    },
+    {
+      id: 6,
+      desc: "",
+      name: "Xiaomi 12 Pro",
+      price: 62999,
+      img: "https://rukminim2.flixcart.com/image/312/312/jpcxrww0/mobile/x/b/b/mi-redmi-6-pro-na-original-imafawhpysnx884c.jpeg?q=70",
+      isSelected: false,
+    },
+    {
+      id: 7,
+      desc: "",
+      name: "Redmi 10",
+      price: 13495,
+      rating: "⭐⭐⭐⭐ 4.1",
+      img: "https://m.media-amazon.com/images/I/31jOF0g5tVL._AC_UL320_.jpg",
+      isSelected: false,
+    },
+    {
+      id: 8,
+      desc: "",
+      name: "OPPO A76",
+      price: 17499,
+      img: "https://m.media-amazon.com/images/I/31QnVF2gn5L._AC_UL320_.jpg",
+      isSelected: false,
+    },
+  ];
+  
+
+  export default data;
